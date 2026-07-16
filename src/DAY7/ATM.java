586e09896bdba34fb36ce259671812ed4b47e109
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:47:13 +0530

    commit

---
+++ src/DAY7/ATM.java
@@ -0,0 +1,35 @@
+package DAY7;
+class Account {
+    String accountNumber;
+    String holderName;
+    double balance;
+    public Account(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+        System.out.println("Account created for " + holderName);
+    }
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.printf("Deposited %.2f. New balance: %.2f%n", amount, balance);
+    }
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else {
+            balance -= amount;
+            System.out.printf("Withdrew %.2f. New balance: %.2f%n", amount, balance);
+        }
+    }
+    public void printStatement() {
+        System.out.printf("[%s] %s -> Balance: %.2f%n", accountNumber, holderName, balance);
+    }
+}
+public class ATM {
+    public static void main(String[] args) {
+        Account acc1 = new Account("AC001", "Asha", 1000);
+        acc1.deposit(500);
+        acc1.withdraw(200);
+        acc1.printStatement();
+    }
+}
