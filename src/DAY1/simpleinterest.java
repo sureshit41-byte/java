3ba2c19c14f5263a0b75b9c76bc1ef2e83d7b740
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 09:24:41 +0530

    commit

---
+++ src/DAY1/simpleinterest.java
@@ -0,0 +1,17 @@
+package DAY1;
+import java.util.Scanner;
+public class simpleinterest {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Principal: ");
+            double p = sc.nextDouble();
+            System.out.print("Enter Rate: ");
+            double r = sc.nextDouble();
+            System.out.print("Enter Time: ");
+            double t = sc.nextDouble();
+
+            double si = (p * r * t) / 100;
+            System.out.println("Simple Interest = " + si);
+        }
+    }
+}
