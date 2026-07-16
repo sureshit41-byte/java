5090a34cdfa6d82288bce7eb4c8d8d86b70a0dfe
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:26:37 +0530

    commit

---
+++ src/DAY5/powerofheroes.java
@@ -0,0 +1,15 @@
+package DAY5;
+
+public class powerofheroes {
+    public int sumOfPower(int[] nums) {
+        long ans = 0;
+        int mod = 1000000007;
+
+        for (int i = 0; i < nums.length; i++) {
+            long power = (long) nums[i] * nums[i] * nums[i];
+            ans = (ans + power) % mod;
+        }
+
+        return (int) ans;
+    }
+}
