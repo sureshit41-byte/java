fc55b3c823898d84a2e614a185af5b19d4b406af
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:40:27 +0530

    commit

---
+++ src/DAY6/consistentstring.java
@@ -0,0 +1,25 @@
+package DAY6;
+
+public class consistentstring {
+    public int countConsistentStrings(String allowed, String[] words) {
+
+        int count = 0;
+
+        for (String word : words) {
+            boolean ok = true;
+
+            for (char c : word.toCharArray()) {
+                if (allowed.indexOf(c) == -1) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
