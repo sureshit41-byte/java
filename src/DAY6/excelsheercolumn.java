a860ffe0667a796e019a1820d71647a9cafc381e
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:38:44 +0530

    commit

---
+++ src/DAY6/excelsheercolumn.java
@@ -0,0 +1,15 @@
+package DAY6;
+public class excelsheercolumn {
+    public String convertToTitle(int columnNumber) {
+
+        StringBuilder sb = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            sb.append((char) ('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+
+        return sb.reverse().toString();
+    }
+}
