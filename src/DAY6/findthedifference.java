5c6bcdd651422d6b2d9ee4e7305a165dfef26576
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:36:27 +0530

    commit

---
+++ src/DAY6/findthedifference.java
@@ -0,0 +1,18 @@
+package DAY6;
+
+public class findthedifference {
+    public char findTheDifference(String s, String t) {
+
+        int sum = 0;
+
+        for (char c : t.toCharArray()) {
+            sum += c;
+        }
+
+        for (char c : s.toCharArray()) {
+            sum -= c;
+        }
+
+        return (char) sum;
+    }
+}
