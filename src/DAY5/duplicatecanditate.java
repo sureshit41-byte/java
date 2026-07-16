15c153abe7d72b0e25a93a1bb3166873e75f425f
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:31:27 +0530

    commit

---
+++ src/DAY5/duplicatecanditate.java
@@ -0,0 +1,18 @@
+package DAY5;
+
+public class duplicatecanditate {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int give = 1;
+        int i = 0;
+
+        while (candies > 0) {
+            ans[i % num_people] += Math.min(give, candies);
+            candies -= give;
+            give++;
+            i++;
+        }
+
+        return ans;
+    }
+}
