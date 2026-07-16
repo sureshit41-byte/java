6e7b4e1062127e114639b8d02bb8d49cacaf6d7b
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 11:28:02 +0530

    commit

---
+++ src/DAY5/duplicatenumber.java
@@ -0,0 +1,16 @@
+package DAY5;
+
+public class duplicatenumber {
+    public int findDuplicate(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+
+        for (int num : nums) {
+            if (visited[num]) {
+                return num;
+            }
+            visited[num] = true;
+        }
+
+        return -1;
+    }
+}
