9757c17f91a1ae450a771e31732ac608f345f6aa
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 10:27:30 +0530

    commit

---
+++ src/DAY4/collectelement.java
@@ -0,0 +1,17 @@
+package DAY4;
+import java.util.List;
+public class collectelement {
+    public int minOperations(List<Integer> nums, int k) {
+        boolean[] seen = new boolean[k + 1];
+        int collected = 0;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int val = nums.get(i);
+            if (val <= k && !seen[val]) {
+                seen[val] = true;
+                collected++;
+                if (collected == k) return nums.size() - i;
+            }
+        }
+        return nums.size();
+    }
+}
