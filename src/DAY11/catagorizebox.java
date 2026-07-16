446401fa8abe60a8ac8e7d7728d64efac1b5068d
sureshit41-byte <syreshit41@gmail.com>
2026-07-16 12:23:24 +0530

    commit

---
+++ src/DAY11/catagorizebox.java
@@ -0,0 +1,28 @@
+package DAY11;
+public class catagorizebox {
+    public String categorizeBox(int length, int width, int height, int mass) {
+
+        boolean bulky = false;
+        boolean heavy = false;
+
+        long volume = (long) length * width * height;
+
+        if (length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000) {
+            bulky = true;
+        }
+
+        if (mass >= 100) {
+            heavy = true;
+        }
+
+        if (bulky && heavy) {
+            return "Both";
+        } else if (bulky) {
+            return "Bulky";
+        } else if (heavy) {
+            return "Heavy";
+        } else {
+            return "Neither";
+        }
+    }
+}
