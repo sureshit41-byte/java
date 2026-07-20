1037b89cd54d98ad1e001cb5d0b07157526bbaeb
sureshit41-byte <syreshit41@gmail.com>
2026-07-20 09:36:36 +0530

    commit

---
+++ src/DAY24/FindDiff.java
@@ -0,0 +1,16 @@
+package DAY24;
+
+public class FindDiff {
+        public char findTheDifference(String s, String t) {
+
+            int sum = 0;
+
+            for (int i = 0; i < t.length(); i++)
+                sum += t.charAt(i);
+
+            for (int i = 0; i < s.length(); i++)
+                sum -= s.charAt(i);
+
+            return (char) sum;
+        }
+    }
