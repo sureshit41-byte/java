7c934911db23d61931186fe5a2819408504e64fe
sureshit41-byte <syreshit41@gmail.com>
2026-07-20 09:34:54 +0530

    commit

---
+++ src/DAY24/UniqueCharacterInString.java
@@ -0,0 +1,19 @@
+package DAY24;
+
+public class UniqueCharacterInString {
+        public int firstUniqChar(String s) {
+
+            int[] count = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                if (count[s.charAt(i) - 'a'] == 1)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
