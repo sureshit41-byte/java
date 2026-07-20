3dc2219dbde319bffb6cb07b5e068de7d48cffd9
sureshit41-byte <syreshit41@gmail.com>
2026-07-20 09:32:34 +0530

    commit

---
+++ src/DAY23/StackUsingQueue.java
@@ -0,0 +1,29 @@
+package DAY23;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class StackUsingQueue {
+        Queue<Integer> q = new LinkedList<>();
+
+        public void push(int x) {
+
+            q.offer(x);
+
+            for (int i = 0; i < q.size() - 1; i++) {
+                q.offer(q.poll());
+            }
+        }
+
+        public int pop() {
+            return q.poll();
+        }
+
+        public int top() {
+            return q.peek();
+        }
+
+        public boolean empty() {
+            return q.isEmpty();
+        }
+    }
