c298e0984b10c61739f6b5bfebe107c41eacad9e
sureshit41-byte <syreshit41@gmail.com>
2026-07-24 14:16:05 +0530

    commit

---
+++ src/DAY26/NumberOfIsland.java
@@ -0,0 +1,37 @@
+package DAY26;
+
+public class NumberOfIsland {
+    public int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(char[][] grid, int r, int c) {
+
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length)
+            return;
+
+        if (grid[r][c] == '0')
+            return;
+
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
