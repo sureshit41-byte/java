43981af0825087f0ac89152f3bd91d8807df818b
sureshit41-byte <syreshit41@gmail.com>
2026-07-24 14:21:02 +0530

    commit

---
+++ src/DAY27/BuildingYouCanReach.java
@@ -0,0 +1,28 @@
+package DAY27;
+import java.util.*;
+public class BuildingYouCanReach {
+
+
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < heights.length - 1; i++) {
+
+            int diff = heights[i + 1] - heights[i];
+
+            if (diff > 0) {
+
+                pq.offer(diff);
+
+                if (pq.size() > ladders)
+                    bricks -= pq.poll();
+
+                if (bricks < 0)
+                    return i;
+            }
+        }
+
+        return heights.length - 1;
+    }
+}
