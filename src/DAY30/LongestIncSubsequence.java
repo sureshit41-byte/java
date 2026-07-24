e0056001a266a8300cc150407ca0a3a164771502
sureshit41-byte <syreshit41@gmail.com>
2026-07-24 14:33:44 +0530

    commit

---
+++ src/DAY30/LongestIncSubsequence.java
@@ -0,0 +1,28 @@
+package DAY30;
+
+public class LongestIncSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            ans = Math.max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+}
